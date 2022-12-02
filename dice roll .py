f64d88815bb75d544d268545e80c8223f609c736
Sid-istic <112649371+Sid-istic@users.noreply.github.com>
2022-12-02 15:32:22 +0530

    Add files via upload

---
+++ dice roll .py
@@ -0,0 +1,49 @@
+import random
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+question = int(input("Please guess a number: "))
+
+for guess in range(question):
+    dice.append(random.randint(1,6))
+print (dice)
+
+for setup in range(question):
+    for visual in dice_art.get(dice[setup]):
+        print (visual)
+
+for die in dice:
+    total = total + die
+print (f"Total: {total}")
